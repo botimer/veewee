6ed5d0fd15163b978c3c32c57c5141fc1f695ed5
Noah Botimer <botimer@umich.edu>
2013-01-31 19:24:20 -0500

    Fix up Chef and Puppet installs
     * Chef prompts for config while installing, so use noninteractive
     * Update the Puppet repository to wheezy packages instead of precise

--- templates/Debian-7.0-b4-amd64-netboot/chef.sh
+++ templates/Debian-7.0-b4-amd64-netboot/chef.sh
@@ -1,4 +1,5 @@
-# Wheezy has a reasonable base Chef, using either Ruby 1.8 or 1.9,
-# so install from packages
-apt-get -y install chef
+# Wheezy has a reasonable base Chef, using either Ruby 1.8 or 1.9, so
+# install from packages. Use noninteractive since we don't care about a
+# remote host for now.
+DEBCONF_FRONTEND=noninteractive apt-get -y install chef
 
--- templates/Debian-7.0-b4-amd64-netboot/puppet.sh
+++ templates/Debian-7.0-b4-amd64-netboot/puppet.sh
@@ -1,7 +1,7 @@
 # Use Puppet Labs' APT repository to install Puppet 3 stable
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-dpkg -i puppetlabs-release-precise.deb
-rm puppetlabs-release-precise.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+dpkg -i puppetlabs-release-wheezy.deb
+rm puppetlabs-release-wheezy.deb
 apt-get update
 apt-get -y install puppet
 
