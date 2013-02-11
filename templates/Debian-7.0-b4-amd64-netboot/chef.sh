74fa3cc2796e3b2efcd0efbac84cff3f3144efe9
Noah Botimer <botimer@umich.edu>
2013-02-11 12:07:04 -0500

    Revert to gem installation for Chef

--- templates/Debian-7.0-b4-amd64-netboot/chef.sh
+++ templates/Debian-7.0-b4-amd64-netboot/chef.sh
@@ -1,5 +1,7 @@
-# Wheezy has a reasonable base Chef, using either Ruby 1.8 or 1.9, so
-# install from packages. Use noninteractive since we don't care about a
-# remote host for now.
-DEBCONF_FRONTEND=noninteractive apt-get -y install chef
+# Wheezy has a reasonable base Chef package, but it is aimed at configuration
+# for a remote host. The gem behaves as expected for Chef Solo.
+
+# Install Chef
+gem install chef --no-ri --no-rdoc
+
 
